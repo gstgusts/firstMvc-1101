4342625cd1b3bf7969be58aa0d1d4d2af396b7d8
Gusts Linkevics <gusts@gst.lv>
2021-02-09 21:35:47 +0200

    Initial commit

---
+++ src/main/java/com/example/firstMvc1101/FirstMvc1101Application.java
@@ -0,0 +1,13 @@
+package com.example.firstMvc1101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMvc1101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMvc1101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstMvc1101/FirstMvc1101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstMvc1101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMvc1101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
